6c716a6f1d495a9396bd5309c435885bae209c64
Garen Sahagian <g.s.sahagian@gmail.com>
2020-03-29 21:14:48 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,56 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+pigeons = data.each_with_object({}) do |(attr_name, attr_values), pigeons|
+  attr_values.each do |attr_value, names|
+    names.each do |name|
+      
+      #if name does not exist in pigeons, create it
+      unless pigeons[name]
+        pigeons[name] = {}
+      end
+
+      #if attribute does not exist under the current name in pigeons, create it
+      unless pigeons[name][attr_name]
+        pigeons[name][attr_name] = []
+      end
+      # if attribute value does not exist within attribute of current name, shovel to array
+      pigeons[name][attr_name] << attr_value.to_s
+    end
+  end
 end
+end
+
+#def nyc_pigeon_organizer(data)
+#  data.each({}) do |attributes, pigeons|
+#    puts "Attributes"
+#    pp attributes
+#    attributes.reduce({}) do |memo, (key, value)|
+#      puts "Attr_Options"
+#      pp attr_options
+#      attr_options.each() do |names, pigeons|
+#        puts "Names"
+#        pp names
+#        string_name = name.toString()
+#        pigeons[string_name] = {}
+#      end
+#    end
+#  end
+#end
+
+#  pp data
+ # data.reduce({}) do |pigeons, (attribute, options)|
+  #  p attribute
+   # p options
+#    value.reduce({}) do | attributes, (options, names)|
+ #     p options
+  #    p names
+   #   value.map(name) do |options, names|
+    #    p name
+     #   p names
+    #    options
+    #  end
+    #  attributes
+  #  end
+  #  pigeons
+#  end
+#end
+
